ed76770594d4e4c9e7c63ad64e762d4bd0e71263
Grace <parkg748@newschool.edu>
2018-08-23 21:32:56 -0700

    Add Stacks, Queue, & Map

--- W1D5/diy_adts.rb
+++ W1D5/diy_adts.rb
@@ -1,4 +1,6 @@
 class Stack
+  attr_accessor :ivar
+
   def initialize
     # create ivar to store stack here!
     @ivar = []
@@ -16,6 +18,66 @@ class Stack
 
   def peek
     # returns, but doesn't remove, the top element in the stack
-    
+    @ivar[-1]
+  end
+end
+
+class Queue
+  attr_accessor :ivar
+
+  def initialize
+    @ivar = []
+  end
+
+  def enqueue(el)
+    @ivar << el
+  end
+
+  def dequeue
+    @ivar.shift
+  end
+
+  def peek
+    @ivar[0]
+  end
+end
+
+class Map
+  attr_accessor :ivar
+
+  def initialize
+    @ivar = []
+  end
+
+  def set(key, value)
+    if @ivar.any? { |x| x.include?(key) }
+      @ivar.each_with_index do |x, i|
+        @ivar[i][1] = value if x[0] == key
+      end
+    else
+      @ivar << [key, value]
+    end
+  end
+
+  def get(key)
+    @ivar.each do |x|
+      value = x[1]
+      return value if x[0] == key
+    end
+  end
+
+  def delete(key)
+    @ivar.delete_if { |x| x[0] == key }
+  end
+
+  def show
+    deep_dup(@ivar)
+  end
+
+  private
+
+  def deep_dup(array)
+    return [array.first] if array.length == 1
+    [array.first] << deep_dup(array[1..-1])
   end
 end
